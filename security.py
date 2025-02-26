810fa744543fdaf42c20c9a6db77c624ab7d3ae9
Ayush Gangwar <arya119000@gmail.com>
2025-02-26 19:47:48 +0530

    Create security.py

---
+++ security.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet
+
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt_file(file_path, key):
+    fernet = Fernet(key)
+    with open(file_path, 'rb') as file:
+        encrypted = fernet.encrypt(file.read())
+    with open(file_path, 'wb') as file:
+        file.write(encrypted)
